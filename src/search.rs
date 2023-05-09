008ee9522c5de14cb4c661daf74ea75589b04a8a
lukakralik <luka.kralik@gmail.com>
2023-05-09 22:55:27 +0200

    search: alternative matching function using the regex externality

--- src/search.rs
+++ src/search.rs
@@ -78,3 +78,44 @@ pub fn suitable_sequences(green: String, yellow: Vec<String>, gray: Vec<String>,
     to_upper(suiting)
     // regex alternative: /\bG\wA*E\wT\b/
 }
+
+// alternative
+// accepts guessed words in the form XX-X-X
+pub fn filter_words(words: Vec<String>, guessed_word: &str, included_letters: &[char], excluded_letters: &[char]) -> Vec<String> {
+    let mut filtered_words = Vec::new();
+
+    // regex pattern
+    let regex_str = guessed_word.replace("-", "[A-Z]");
+    let regex = Regex::new(&regex_str).unwrap();
+
+    for word in words {
+        // this situation shouldn't be reached if the correct data was passed
+        if word.len() != guessed_word.len() {
+            continue;
+        }
+        // doesn't fit in the RegEx pattern
+        if !regex.is_match(word.as_ref()) {
+            continue;
+        }
+        // see if word contains the mandatory included characters
+        let mut all_incl_letters = true;
+        for &letter in included_letters {
+            if !word.contains(letter) {
+                all_incl_letters = false;
+                break;
+            }
+        }
+        if !all_incl_letters {
+            continue;
+        }
+        // erase the word if it contains forbidden characters
+        for &letter in excluded_letters {
+            if word.contains(letter) {
+                continue;
+            }
+        }
+        filtered_words.push(word.to_owned());
+    }
+
+    filtered_words
+}
