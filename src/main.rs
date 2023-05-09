7396c5d85db05e415b03fb0bd60702ba33120c14
lukakralik <luka.kralik@gmail.com>
2023-05-09 22:55:45 +0200

    verification for the regex matching function

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::{
     env::current_dir,
     io::stdin,
 };
+use crate::search::*;
 
 pub use crate::utils::*;
 pub use crate::search::*;
@@ -13,8 +14,13 @@ pub mod search;
 
 fn main() {
     let data = import_dataset("test", 0);
+    let data2 = data.clone();
     println!("{:#?}", data);
 
     let res = suitable_sequences(String::from("G*T*"), vec![String::from("A"), String::from("E")], vec![String::from("B")], data);
     println!(" res {:?}", res);
+    
+    // add testcase for the filter alternative
+    let result = filter_words(data2, "G-T-", &['A', 'T'], &['X', 'Y']);
+    println!("{:#?}", result);
 }
