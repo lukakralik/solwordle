c6981c08f1df274ad6b9ae24eb6b34df75f2c328
Luka Králík <luka.kralik@gmail.com>
2023-06-16 11:33:32 +0200

    i have schizophrenia

--- web/script.js
+++ web/script.js
@@ -1,12 +1,9 @@
-var rowCounter = 1;
-
 function generateBoxes() {
-    // get the word length input value
     var wordLength = document.getElementById("word-length-input").value;
     var boxContainer = document.getElementById("word-box-container");
     boxContainer.innerHTML = "";
 
-    // dynamically update the number of boxes and color pickers when the length is updated
+    // dynamically update the number of boxes when the length is updated
     for (var i = 0; i < wordLength; i++) {
         var input = document.createElement("input");
         input.className = "word-box";
@@ -14,24 +11,12 @@ function generateBoxes() {
         input.maxLength = 1;
         input.addEventListener("input", moveCursor);
 
-        // create new color input for each letter and assign gray as the default color
-        var colorInput = document.createElement("input");
-        colorInput.className = "color-picker";
-        colorInput.type = "color";
-        colorInput.setAttribute("list", "color-options");
-        colorInput.value = "#a4aec4";
-
-        // create a pair of inputs
-        var boxWrapper = document.createElement("div");
-        boxWrapper.className = "word-box-wrapper";
-        boxWrapper.appendChild(input);
-        boxWrapper.appendChild(colorInput);
-
-        boxContainer.appendChild(boxWrapper);
+        boxContainer.appendChild(input);
     }
 }
 
-// TODO: move cursor to the next letter in the row after entering the current one
+// move the cursor to the next box after a letter is entered
+// move one box back in case backspace is pressed
 function moveCursor(event) {
     var input = event.target;
     var keyCode = event.keyCode || event.which;
@@ -46,7 +31,9 @@ function moveCursor(event) {
             allInputs[allInputs.length - 1].focus();
         }
     } else if (input.value.length === 1) {
+        // Capitalize the letter
         input.value = input.value.toUpperCase();
+
         var nextInput = input.nextElementSibling;
         if (nextInput !== null) {
             nextInput.focus();
@@ -54,16 +41,15 @@ function moveCursor(event) {
     }
 }
 
-// check the correctness of the last guess and generate new line
+// ran each time the check button is pressed, process the existing results
+// go to the next line and continue
 function checkGuess() {
     var wordLength = document.getElementById("word-length-input").value;
-    var boxContainer = document.getElementById("word-box-container");
 
-    // create a new line container for the current pair
+    // create a div for the new entry
     var lineDiv = document.createElement("div");
-    lineDiv.className = "line";
 
-    // generate the input boxes and color pickers
+    // generate the input boxes
     for (var i = 0; i < wordLength; i++) {
         var input = document.createElement("input");
         input.className = "word-box";
@@ -71,43 +57,16 @@ function checkGuess() {
         input.maxLength = 1;
         input.addEventListener("input", moveCursor);
 
-        var colorInput = document.createElement("input");
-        colorInput.className = "color-picker";
-        colorInput.type = "color";
-        colorInput.setAttribute("list", "color-options");
-        colorInput.value = "#a4aec4";
-
-        var boxWrapper = document.createElement("div");
-        boxWrapper.className = "word-box-wrapper";
-        boxWrapper.appendChild(input);
-        boxWrapper.appendChild(colorInput);
-
-        lineDiv.appendChild(boxWrapper);
+        lineDiv.appendChild(input);
     }
 
-    // insert the new line container below the current pair
-    var existingLines = document.getElementsByClassName("line");
-    if (existingLines.length > 0) {
-        boxContainer.insertBefore(lineDiv, existingLines[existingLines.length - 1].nextSibling);
-    } else {
-        boxContainer.appendChild(lineDiv);
-    }
+    var boxContainer = document.getElementById("word-box-container");
+    boxContainer.appendChild(lineDiv);
 
-    // focus on the first input of the new line
-    var firstInput = lineDiv.querySelector(".word-box");
+    // focus on the first box in the new line
+    var firstInput = lineDiv.getElementsByTagName("input")[0];
     firstInput.focus();
-
-    // reset the input values and color pickers for the next line
-    var wordBoxes = lineDiv.querySelectorAll(".word-box");
-    var colorPickers = lineDiv.querySelectorAll(".color-picker");
-
-    // TODO: find a better way to construct an array from the paired letters and colors
-    Array.from(wordBoxes).forEach(input => (input.value = ""));
-    Array.from(colorPickers).forEach(colorPicker => (colorPicker.value = "#a4aec4"));
-
-    // TODO: process the guessed word and colors here
-    processGuess(wordBoxes, colorPickers);
 }
 
-// call the generateBoxes() function initially
+// boxes from the default value
 generateBoxes();
