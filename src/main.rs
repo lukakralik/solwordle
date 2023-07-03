b52e7b843adea9505772e394412b7c4a5422cffd
Luka Králík <luka.kralik@gmail.com>
2023-07-03 21:33:31 +0200

    prepare for server and CLI branching

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-#![feature(decl_macro)]
-#[macro_use]
-extern crate rocket;
-extern crate rocket_cors;
+#[cfg(feature = "server")]
+mod server {
+    #![feature(decl_macro)]
+    #[macro_use]
+    extern crate rocket;
+    extern crate rocket_cors;
 
-use rocket::ignite;
-use rocket_cors::{AllowedOrigins, CorsOptions};
-use std::env::current_dir;
+    use rocket::ignite;
+    use rocket_cors::{AllowedOrigins, CorsOptions};
+    use std::env::current_dir;
+}
+
+#[cfg(not(feature = "server"))]
+mod cli {
+    {}
+}
 
 pub use crate::utils::*;
 pub use crate::search::*;
@@ -15,14 +23,20 @@ pub mod routes;
 pub mod models;
 
 fn main() {
-    println!("{:#?}", current_dir());
-    let cors = CorsOptions::default()
-        .allowed_origins(AllowedOrigins::all())
-        .to_cors()
-        .expect("Failed to initialize CORS");
+    #[cfg(feature = "server")] {
+        println!("{:#?}", current_dir());
+        let cors = CorsOptions::default()
+            .allowed_origins(AllowedOrigins::all())
+            .to_cors()
+            .expect("Failed to initialize CORS");
 
-    ignite()
-        .mount("/", routes![routes::fitting_sequence])
-        .attach(cors)
-        .launch();
+        ignite()
+            .mount("/", routes![routes::fitting_sequence])
+            .attach(cors)
+            .launch();
+    }
+
+    #[cfg(feature = "cli")] {
+        {}
+    }
 }
